f01fe4192334ef1d9d59797f1e483792d91358cc
mrmoris <mrmoris78@gmail.com>
2024-03-04 18:28:05 +0530

    new update commit 4

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
 print("Heloo for test file")
 print("Heloo from github")
-<<<<<<< HEAD
-print("Heloo for test file")
-print("this line no 2")
-=======
 print("this is line no 3")
->>>>>>> 5385786437ae33c45746fb80147135930423cc74
